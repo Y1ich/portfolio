a5b4172a86fb45ef8abba74eb6363db9eed1b08c
Yulia Sidorenko <131293279+Y1ich@users.noreply.github.com>
2023-08-02 18:19:05 +0300

    Add files via upload

---
+++ lab/Code10/Code10/Code10.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class ExceptionSize // Класс исключения
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Слишком большой запрашиваемый объем памяти";
+	}
+};
+
+class Vector
+{
+private:
+	int size;  // Фактический объем памяти
+	int* data; // Указатель на динамический массив
+
+public:
+	enum { max = 1000 };
+
+	Vector(int size) //конструктор
+	{
+		if (size<0 || size>max)
+			throw ExceptionSize();
+		data = new int[size];;
+	}
+
+	~Vector() //деструктор
+	{
+		delete[] data;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int size;
+	cin >> size;
+	try
+	{
+		Vector v(size);
+		cout << "Выделена память: " << size << endl;
+	}
+	catch (const ExceptionSize& e)
+	{
+		cout << e.what() << endl;
+	}
+	return 0;
+}
+
